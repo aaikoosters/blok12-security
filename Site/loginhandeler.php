6d0a46f87367d197d99fc8ecc331c6a12f0fb3a5
Habahabahababa <jeroenhoebert@gmail.com>
2018-11-22 14:39:00 +0100

    Sessie token & pw hash

--- Site/loginhandeler.php
+++ Site/loginhandeler.php
@@ -1,14 +1,16 @@
 <?php
 
+$hcpw = password_hash("2", PASSWORD_BCRYPT, ['cost' => 12]); //12 is cost, 2 is pw
+
 $hcun = 1;
-$hcpw = 2;
+
 SESSION_START();
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 if ($username == $hcun)
 {
-    if ($password == $hcpw)
+    if (password_verify($password, $hcpw))
     {
         header('Location: portal.html');
     }
