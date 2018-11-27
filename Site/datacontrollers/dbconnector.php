a8a7ed7da06aedd35fe4acd7952dca1f96c883a4
Habahabahababa <jeroenhoebert@gmail.com>
2018-11-27 16:35:18 +0100

    databaseconnector update

--- Site/datacontrollers/dbconnector.php
+++ Site/datacontrollers/dbconnector.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "1234";
+$password = "JkCzA1eXa4NBxFwRDnPLbGykHS2KzXSbD9JMZIrrVDlUhat058rHu6RVVLqpMDEWltKnnbdRK9QFf9JE7RvoJjDSDTD05i1U9oVMckW0cdz2vS9L8jbV6hRcKVHA4akSsCY8zYRy4iLsNGEYD9XD1NSdy0GBvuRCHv5TPbwE7uEvNyAOazMr0DaDhYduNnl8HkbTEhhm37mMeaWBvDNVtUdmNXOmJ8Ka9fILwUsiLaHdQiO3HHdyhz8Zf0388exo";
 $dbname = "blok12db";
 
 // Create connection
